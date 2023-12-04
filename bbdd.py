9a6192c524c8c6d3ea887fa842af1506b708c739
Joan <jcollgu@gmail.com>
2023-12-04 15:47:37 +0100

    Changes do fix api errors

--- bbdd.py
+++ bbdd.py
@@ -11,26 +11,32 @@ try:
             url = f'https://api.spoonacular.com/recipes/{bd[1]}/information?apiKey=097afe44aca74010895efa6139eba2b5'
             res = requests.get(url)
             data = res.json()
-            if not data.get('image'):
-                data['image'] = ''
-            ingr = ''
-            for ingrs in data['extendedIngredients']:
-                ingr = f'{ingr} / {ingrs["originalName"]}'
-            ingr.strip()
-            ingr = re.sub(r'\"', '', ingr)
-            # print(ingr)
-            string = 'INSERT INTO bbdd (id_recipe, name, ingredients, minutes, servings, instructions, vegan, vegetarian, glutenFree, diaryFree, image) VALUES(%d, "%s", "%s", %d, %d, "%s", %s, %s, %s, %s, "%s");' % (data["id"], data["title"], ingr, data["readyInMinutes"], data["servings"], data["instructions"], data["vegan"], data["vegetarian"], data["glutenFree"], data["dairyFree"], data["image"])
-            # print(string)
-            cur.execute(string)
-            conn.commit()
-            for insts in data['analyzedInstructions'][0]['steps']:
-                ingredient = ''
-                for ingredients in insts['ingredients']:
-                    ingredient = f'{ingredient}, {ingredients["name"]}'
-                cur.execute(f'INSERT INTO recipe_steps (recipe_id, step, instruction, ingredients) VALUES({bd[1]}, {insts["number"]}, "{insts["step"]}", "{ingredient}");')
+            if not data.get('instructions'):
+                data['instructions'] = None
+            if data['instructions'] != None:
+                if not data.get('image'):
+                    data['image'] = ''
+                ingr = ''
+                for ingrs in data['extendedIngredients']:
+                    ingr = f'{ingr} / {ingrs["originalName"]}'
+                ingr.strip()
+                ingr = re.sub(r'\"', '', ingr)
+                ins = re.sub(r'\"', '', data['instructions'])
+                title = re.sub(r'\"', '', data['title'])
+                string = 'INSERT INTO bbdd (id_recipe, name, ingredients, minutes, servings, instructions, vegan, vegetarian, glutenFree, diaryFree, image) VALUES(%d, "%s", "%s", %d, %d, "%s", %s, %s, %s, %s, "%s");' % (data["id"], title, ingr, data["readyInMinutes"], data["servings"], ins, data["vegan"], data["vegetarian"], data["glutenFree"], data["dairyFree"], data["image"])
+                # print(string)
+                cur.execute(string)
                 conn.commit()
-            cur.execute(f'UPDATE db_sencilla SET done = true WHERE id = {bd[0]};')
-            conn.commit()
+                for insts in data['analyzedInstructions'][0]['steps']:
+                    ingredient = ''
+                    for ingredients in insts['ingredients']:
+                        step = re.sub(r'\"', '', insts["step"])
+                        ingredient = f'{ingredient}, {ingredients["name"]}'
+                        ingredient = re.sub(r'\"', '', ingredient)
+                    cur.execute(f'INSERT INTO recipe_steps (recipe_id, step, instruction, ingredients) VALUES({bd[1]}, {insts["number"]}, "{step}", "{ingredient}");')
+                    conn.commit()
+                cur.execute(f'UPDATE db_sencilla SET done = true WHERE id = {bd[0]};')
+                conn.commit()
 except Exception as e:
     print(e)
 
