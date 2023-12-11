99acd7f9d758ce0d0400f0322a0e41313bdfea5d
Joan <jcollgu@gmail.com>
2023-12-11 17:53:11 +0100

    Changes to table

--- proyecto1.py
+++ proyecto1.py
@@ -78,48 +78,133 @@ def abrir_ventana_secundaria():
     ventana_secundaria.config(width=300, height=200)
 
     if option.get() == 1:
-        condition = 'WHERE vegan = 1'
+        condition = 'AND vegan = 1'
     elif option.get() == 2:
-        condition = 'WHERE vegetarian = 1'
+        condition = 'AND vegetarian = 1'
     elif option.get() == 3:
-        condition = 'WHERE diaryFree = 1'
+        condition = 'AND diaryFree = 1'
     elif option.get() == 4:
-        condition = 'WHERE glutenFree = 1'
+        condition = 'AND glutenFree = 1'
     elif option.get() == 5:
         condition = ''
 
-    listLunch = ['Almuerzo']
-    listBreakfast = ['Desayuno']
-    listDinner = ['Cena']
-    listSnack = ['Merienda']
 
-    ListDishes = ['breakfast' ,'main course', 'snack', 'main course']
+    listDishes = ['breakfast' ,'main course', 'snack', 'main course']
+    listDays = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+    dicIds = dict()
 
-    table = ttk.Treeview(ventana_secundaria, columns=('', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'), show='headings')
-    table.heading('Lunes', text = 'Lunes')
-    table.heading('Martes', text = 'Martes')
-    table.heading('Miercoles', text = 'Miercoles')
-    table.heading('Jueves', text = 'Jueves')
-    table.heading('Viernes', text = 'Viernes')
-    table.heading('Sabado', text = 'Sabado')
-    table.heading('Domingo', text = 'Domingo')
-    table.pack(fill = 'both', expand = True)
-
-    for i in range(4):
-        condition = condition + f' AND type = "{ListDishes[i]}"'
-        print(condition)
-        for j in range(8):
-            cur.execute(f'SELECT id FROM `bbdd` {condition} ORDER BY RAND() LIMIT 1')
+    for day in listDays:
+        for dish in listDishes:
+            cur.execute(f'SELECT id FROM `bbdd` WHERE type = "{dish}" {condition} ORDER BY RAND() LIMIT 1')
             id = cur.fetchall()[0][0]
-            cur.execute(f'SELECT name FROM bbdd WHERE id = {id}')
-            name = cur.fetchall()[0][0]
-            print(name)
-            listLunch.append(name)
-            
-        table.insert(parent='', index=i, values=(listLunch))
+            if dicIds.get(f'{day}_{dish}'):
+                dicIds[f'{day}_{dish}2'] = id
+            else:
+                dicIds[f'{day}_{dish}'] = id
+    # print(dicIds)
 
-    
-    
+    def details(button):
+        # print(dicIds[button])
+        ventana_detalles = Toplevel()
+        ventana_detalles.title("Details")
+        ventana_detalles.config(width=300, height=200)
+
+        cur.execute(f'SELECT name FROM `bbdd` WHERE id = {dicIds[button]}')
+        name = cur.fetchall()[0][0]
+        print(name)
+
+        pass
+
+    button_lunes_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('lunes_main course'))
+    button_martes_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('martes_main course'))
+    button_miercoles_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('miercoles_main course'))
+    button_jueves_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('jueves_main course'))
+    button_viernes_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('viernes_main course'))
+    button_sabado_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('sabado_main course'))
+    button_domingo_main1 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('domingo_main course'))
+
+    button_lunes_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('lunes_main course2'))
+    button_martes_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('martes_main course2'))
+    button_miercoles_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('miercoles_main course2'))
+    button_jueves_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('jueves_main course2'))
+    button_viernes_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('viernes_main course2'))
+    button_sabado_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('sabado_main course'))
+    button_domingo_main2 = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('domingo_main course'))
+
+    button_lunes_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('lunes_breakfast'))
+    button_martes_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('martes_breakfast'))
+    button_miercoles_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('miercoles_breakfast'))
+    button_jueves_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('jueves_breakfast'))
+    button_viernes_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('viernes_breakfast'))
+    button_sabado_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('sabado_breakfast'))
+    button_domingo_breakfast = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('domingo_breakfast'))
+
+    button_lunes_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('lunes_snack'))
+    button_martes_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('martes_snack'))
+    button_miercoles_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('miercoles_snack'))
+    button_jueves_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('jueves_snack'))
+    button_viernes_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('viernes_snack'))
+    button_sabado_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('sabado_snack'))
+    button_domingo_snack = Button(ventana_secundaria, width=10, text='Detalles', command=lambda:details('domingo_snack'))
+
+    button_lunes_breakfast.grid(row=1, column=1)
+    button_martes_breakfast.grid(row=1, column=2)
+    button_miercoles_breakfast.grid(row=1, column=3)
+    button_jueves_breakfast.grid(row=1, column=4)
+    button_viernes_breakfast.grid(row=1, column=5)
+    button_sabado_breakfast.grid(row=1, column=6)
+    button_domingo_breakfast.grid(row=1, column=7)
+
+    button_lunes_main1.grid(row=2, column=1)
+    button_martes_main1.grid(row=2, column=2)
+    button_miercoles_main1.grid(row=2, column=3)
+    button_jueves_main1.grid(row=2, column=4)
+    button_viernes_main1.grid(row=2, column=5)
+    button_sabado_main1.grid(row=2, column=6)
+    button_domingo_main1.grid(row=2, column=7)
+
+    button_lunes_snack.grid(row=3, column=1)
+    button_martes_snack.grid(row=3, column=2)
+    button_miercoles_snack.grid(row=3, column=3)
+    button_jueves_snack.grid(row=3, column=4)
+    button_viernes_snack.grid(row=3, column=5)
+    button_sabado_snack.grid(row=3, column=6)
+    button_domingo_snack.grid(row=3, column=7)
+
+    button_lunes_main2.grid(row=4, column=1)
+    button_martes_main2.grid(row=4, column=2)
+    button_miercoles_main2.grid(row=4, column=3)
+    button_jueves_main2.grid(row=4, column=4)
+    button_viernes_main2.grid(row=4, column=5)
+    button_sabado_main2.grid(row=4, column=6)
+    button_domingo_main2.grid(row=4, column=7)
+
+    label_lunes = Label(ventana_secundaria, text='Lunes')
+    label_martes = Label(ventana_secundaria, text='Martes')
+    label_miercoles = Label(ventana_secundaria, text='Miercoles')
+    label_jueves = Label(ventana_secundaria, text='Jueves')
+    label_viernes = Label(ventana_secundaria, text='Viernes')
+    label_sabado = Label(ventana_secundaria, text='Sabado')
+    label_domingo = Label(ventana_secundaria, text='Domingo')
+
+    label_desayuno = Label(ventana_secundaria, text='Desayuno')
+    label_almuerzo = Label(ventana_secundaria, text='Almuerzo')
+    label_snack = Label(ventana_secundaria, text='Merienda')
+    label_cena = Label(ventana_secundaria, text='Cena')
+
+    label_lunes.grid(row=0, column=1)
+    label_martes.grid(row=0, column=2)
+    label_miercoles.grid(row=0, column=3)
+    label_jueves.grid(row=0, column=4)
+    label_viernes.grid(row=0, column=5)
+    label_sabado.grid(row=0, column=6)
+    label_domingo.grid(row=0, column=7)
+
+    label_desayuno.grid(row=1, column=0)
+    label_almuerzo.grid(row=2, column=0)
+    label_snack.grid(row=3, column=0)
+    label_cena.grid(row=4, column=0)
+
 
 generador= Button(dias_frame, text="Generar menú semanal",font=("Verdana",28),command=abrir_ventana_secundaria)
 generador.pack()
